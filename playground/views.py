95535ffbda7f5f8962811a00b36bf38a7399cdd1
Horlamide <olamide.jubril@taxtech.com.ng>
2021-10-31 21:43:19 +0100

    [Model Serializers]

--- playground/views.py
+++ playground/views.py
@@ -8,12 +8,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, OrderItem
 
 def say_hello(request):
-    # Solution: 1
-    ordered_items = OrderItem.objects.values_list('product__id').distinct()
-    queryset = Product.objects.filter(id__in=ordered_items).order_by('title')
-
-    # Solution: 2
-    queryset = Product.objects.filter(
-        id__in=OrderItem.objects.values_list('product__id')).order_by('title')
+    queryset = OrderItem.objects.all()
 
     return render(request, 'hello.htm', {"products": list(queryset)})
