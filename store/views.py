14216931ba990b78939f78269e36cb1b97dabcde
Horlamide <olamide.jubril@taxtech.com.ng>
2021-11-03 15:12:08 +0100

    [Class based view] re-factoring init function based view to class based view

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from . models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
